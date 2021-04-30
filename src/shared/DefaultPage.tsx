d427144d7334e7dca6344f7c8061c71eb9d913dd
Rylan McAlister <55506412+RylanMcA@users.noreply.github.com>
2021-04-30 10:55:25 -0500

    Testing new ideas

--- src/shared/DefaultPage.tsx
+++ src/shared/DefaultPage.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Grid, Button } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(2),
+    },
+  })
+);
 
 const DefaultPage = () => {
-
-    return(
-        <div>testing a route</div>
-    );
-}
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid
+        container
+        className={classes.root}
+        direction='row'
+        justify='space-evenly'
+        alignItems='center'
+      >
+        {[0, 1, 2, 3].map((value) => (
+          <Grid item key={value}>
+            <Button variant='contained' color='primary'>
+              {value}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
 
 export default DefaultPage;
