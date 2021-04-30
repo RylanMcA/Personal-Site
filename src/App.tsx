0369a5bf7c07b3f5d6fcffdf89de489a4f92cdfa
Rylan McAlister <55506412+RylanMcA@users.noreply.github.com>
2021-04-30 11:20:38 -0500

    Moving files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
-import DefaultPage from './shared/DefaultPage';
+import DefaultPage from './pages/DefaultPage';
 
 function App() {
   return (
--- src/pages/DefaultPage.tsx
+++ src/pages/DefaultPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Grid, Button } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import GitHubIcon from '@material-ui/icons/GitHub';
 
+//------------------
+//       CSS
+//------------------
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -10,6 +14,10 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+
+//------------------
+// HTML + TypeScript
+//------------------
 const DefaultPage = () => {
   const classes = useStyles();
   return (
@@ -21,7 +29,7 @@ const DefaultPage = () => {
         justify='space-evenly'
         alignItems='center'
       >
-        {[0, 1, 2, 3].map((value) => (
+        {[1,2,3,4].map((value) => (
           <Grid item key={value}>
             <Button variant='contained' color='primary'>
               {value}
